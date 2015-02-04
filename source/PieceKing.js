08f80500036a574aa3d257b6a1155e24b1c8a3a9
ahurdis <ahurdis@hotmail.com>
2015-02-03 22:28:13 -0500

    Added basic move to PieceKing

--- source/PieceKing.js
+++ source/PieceKing.js
@@ -33,8 +33,41 @@ define(["prototypes",
 
             self.getMoves = function (arrayMoves, arrayMatrix) {
 
+                // TODO : Once other piece movement is written, need to look for Check!
+                // TODO : Castling
+
                 try {
 
+
+                    for (var iDx = -1; iDx <= 1; iDx++) {
+
+                        for (var iDy = -1; iDy <= 1; iDy++) {
+
+                            if (iDx === 0 && iDy === 0) {
+                                continue;
+                            }
+
+                            var iXMove = self.x + iDx;
+                            var iYMove = self.y + iDy;
+
+                            if (iXMove > 0 && iXMove < 8 && iYMove > 0 && iYMove < 8) {
+
+                                var pieceMatrix = arrayMatrix[iXMove][iYMove];
+
+                                if (pieceMatrix && self.white !== pieceMatrix.white) {
+
+                                    arrayMoves.push({
+
+                                        piece: self,
+                                        x: iXMove,
+                                        y: iYMove,
+                                        pieceRemove: pieceMatrix
+                                    });
+                                }
+                            }
+                        }
+                    }
+                    // Capture
 
                     return null;
                 } catch (e) {
